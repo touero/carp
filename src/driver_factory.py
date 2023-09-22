cb82cd21bcbe08ba2b0d114ac2a19f77cda43834
touEr_ <61647484+weiensong@users.noreply.github.com>
2023-09-22 14:31:51 +0800

    feat: waitting element disappear by xpath and Non-blocking access

--- src/driver_factory.py
+++ src/driver_factory.py
@@ -13,8 +13,11 @@ class DriverFactory:
             ChromiumPage(addr_driver_opts=CHROME_ADDRESS)
             self.options.add_experimental_option("debuggerAddress", CHROME_ADDRESS)
         else:
+            # self.options.add_argument('start-fullscreen')
             self.options.add_argument("disable-blink-features=AutomationControlled")
             self.options.add_experimental_option("excludeSwitches", ['enable-automation'])
             self.options.add_experimental_option('detach', DEBUG)
+            self.options.page_load_strategy = 'none'
+            
         self.driver = Chrome(options=self.options, service=self.service)
 
